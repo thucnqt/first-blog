628866151b0f8a863ca85cfe669f25482c9d7038
Knowledge <thucnqt@gmail.com>
2020-03-09 14:07:55 +0700

    update

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -2,12 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from .views import index, blog
+from post.views import index, blog, post
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('blog/', blog),
+    path('post/', post),
 ]
 
 if settings.DEBUG:
--- src/post/views.py
+++ src/post/views.py
@@ -5,3 +5,5 @@ def index(request):
     return render(request, 'index.html', {})
 def blog(request):
     return render(request, 'blog.html', {})
+def post(request):
+    return render(request, 'post.html', {})
