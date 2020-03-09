c48f2745651e3e87606a1988226c8723baac3e26
Knowledge <thucnqt@gmail.com>
2020-03-09 11:14:25 +0700

    First Blog

---
+++ src/blog/urls.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+from .views import index, blog
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index),
+    path('blog/', blog),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
